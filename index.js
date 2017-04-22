14e4c2857435e27a795dc06e93e3a1cc2b26dd56
Leandro Alonso <lalonso@thoughtworks.com>
2017-04-21 23:46:58 -0300

    Add 404 and internal error handling

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ app.get('/api', (req, res) => {
   res.send('API is running')
 })
 
+app.use((req, res, next) => {
+  res.status(404)
+  log.debug('Not found URL: %s', req.url)
+  res.send({ error: 'Not found' })
+  return
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  log.error('Internal error(%d): %s', res.statusCode, err.message)
+  res.send({ error: err.message })
+  return
+})
+
 app.listen(1337, () => {
   log.info('Express server listening on port 1337')
 })
