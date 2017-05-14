30f4b2fcfe41f30582c045a66958bd5c8c5905c4
Leandro Alonso <lalonso@thoughtworks.com>
2017-05-14 13:17:24 -0300

    Adds a strutcture to tests

---
+++ test/test-index.js
@@ -0,0 +1,12 @@
+const sinon = require('sinon')
+const chai = require('chai')
+const expect = chai.expect
+
+const mongoose = require('mongoose')
+require('sinon-mongoose')
+
+describe('A Describe', () => {
+  it('tests', () => {
+  
+  })
+})
