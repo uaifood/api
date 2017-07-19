516818fbab9c02dfe50b7869351fb9cd0a9e7a13
Leandro Alonso <lalonso@thoughtworks.com>
2017-07-19 19:36:23 -0300

    Fixes the Restaurant model

--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const Restaurant = new Schema({
+    _id: { type: String },
     name: { type: String, required: true },
     phone: { type: Number },
     deliveryFee: { type: Number },
